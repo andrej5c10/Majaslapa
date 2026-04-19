7a625e32f430abf38ba1a7c943596b547e166883
andrej5c10 <andrejpugacev253@gmail.com>
2026-04-19 22:10:40 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -16,3 +16,38 @@ const prieksmeti = {
         alert("Lūdzu, ievadiet pareizu priekšmeta nosaukumu!");
     }
 }
+
+
+
+function random_int(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+var list_of_questions = [
+    "What is 2+2?",
+    "What is the capital of Great Britain?",
+    "Cik burti latviešu valodā alfbetā?"
+];
+var list_of_answers = [
+    "4", 
+    "London",
+    "27"
+];
+
+const NUMBER_OF_QUESTIONS = 3;
+
+function next_question() {
+    const question = document.getElementById("question");
+    const answer = document.getElementById("answer");
+
+    const question_id = random_int(0, NUMBER_OF_QUESTIONS - 1);
+
+    question.innerHTML = list_of_questions[question_id];
+    answer.innerHTML = list_of_answers[question_id];
+    answer.style.display = "none";
+}
+
+function show_answer() {
+    const answer = document.getElementById("answer");
+    answer.style.display = "block";
+}
